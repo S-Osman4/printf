5b15d03bd162a59b591d583311661ad23b2d60ee
Morr-O <morogara5@gmail.com>
2022-04-18 12:06:18 -0700

    task 0 on printf functions

--- test/strlen.c
+++ test/strlen.c
@@ -1,10 +1,16 @@
 #include "main.h"
+#include <stdarg.h>
 #include <stdio.h>
-
 int _printf(const char *format, ...)
 {
+	int count=0;
+	va_list args;
+	va_start(args, format);
+
+		while(*(format+count) != '\0')
+			count++;
+	va_end(args);
 	
-	printf("%c\n", format);
-	return 0;
+	return (count);
 	
 }
