20083fe1b014c2b286b1f3dba2975853f32c47cf
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-19 23:17:09 +0300

    change back to original

--- _printf.c
+++ _printf.c
@@ -27,12 +27,12 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			if (format[i] == 'c' || format[i] == 's')
-				chars_printed += format[i] == 'c' ? _putchar((char c)va_arg(args, int)) :
+				chars_printed += format[i] == 'c' ? _putchar(va_arg(args, int)) :
 				print_string(va_arg(args, char *));
 			else if (format[i] == 'd' || format[i] == 'i')
-				chars_printed += print_num((long int n)va_arg(args, int));					
+				chars_printed += print_num(va_arg(args, int));					
 			else
-				chars_printed += print_unknown_spec((char c)format[i]);
+				chars_printed += print_unknown_spec(format[i]);
 		}
 		i++;
 	}
