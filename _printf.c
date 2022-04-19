79cde9bd7f85330ed06701aebbcfcdde935ef297
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-19 23:08:00 +0300

    dd changes

--- _printf.c
+++ _printf.c
@@ -30,20 +30,7 @@ int _printf(const char *format, ...)
 				chars_printed += format[i] == 'c' ? _putchar(va_arg(args, int)) :
 				print_string(va_arg(args, char *));
 			else if (format[i] == 'd' || format[i] == 'i')
-				chars_printed += print_num(va_arg(args, int));
-			else if (format[i] == 'b')
-				chars_printed += print_binary((unsigned int)va_arg(args, int));
-			else if (format[i] == 'r')
-				chars_printed += print_reverse(va_arg(args, char *));
-			else if (format[i] == 'R')
-				chars_printed += print_rot13(va_arg(args, char *));
-			else if (format[i] == 'o' || format[i] == 'u' ||
-			format[i] == 'x' || format[i] == 'X')
-				chars_printed += print_odh(format[i], (unsigned int)va_arg(args, int));
-			else if (format[i] == 'S')
-				chars_printed += print_S(va_arg(args, char *));
-			else if (format[i] == 'p')
-				chars_printed += print_pointer(va_arg(args, void *));
+				chars_printed += print_num(va_arg(args, int));					
 			else
 				chars_printed += print_unknown_spec(format[i]);
 		}
