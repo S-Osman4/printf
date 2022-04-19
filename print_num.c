7e4a970322e8277ff486c41e53b3ad6eaea46a91
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-19 23:25:14 +0300

    Update print_num.c

--- print_num.c
+++ print_num.c
@@ -1,49 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_num - prints the digits of an interger
- * @n: integer to be printed
- * Return: no return value
+ * print_number - prints an integer
+ * @n: the integer to print
+ * Return: the number of printed digit
  */
-int print_num(long int n)
+int print_number(int n)
 {
-unsigned long int x, base;
-int chars_printed = 0;
+	int positive_value, number_of_digit = 1;
+	int divider, i, j, abs_value, count = 0;
 
-x = 0;
-base = 10;
-if (n < 0)
-{
-chars_printed += _putchar(45);
-x = -n;
-}
-else
-{
-x = n;
-}
-if (x < base)
-{
-chars_printed += _putchar('0' + x);
-}
-else
-{
-while (x >= base)
-{
-base *= 10;
-if (base == 1000000000000000000)
-break;
-}
-if (!(x > 1000000000000000000))
-base /= 10;
-chars_printed += _putchar('0' + (x / base));
-base /= 10;
-while (base >= 10)
-{
-chars_printed += _putchar('0' + ((x / base) % 10));
-base /= 10;
-}
-chars_printed += _putchar('0' + (x % 10));
-}
-return (chars_printed);
+	/* print the sign and get the absolute value*/
+	if (n < 0)
+	{
+		count += _putchar('-');
+		if (n ==  -2147483648)
+		{
+			count += _putchar('0' + 2);
+			abs_value = positive_value = 147483648;
+		}
+		else
+			abs_value = positive_value = -n;
+	}
+	else
+		abs_value = positive_value = n;
+	/* Count the number of digit */
+	while (positive_value > 9)
+	{
+		number_of_digit++;
+		positive_value /= 10;
+	}
+	for (i = number_of_digit; i > 0; i--)
+	{/*print the number starting by his first digit*/
+		divider = 1;
+		j = 1;
+		while (j < i)
+		{/* calculate the power divider = pow(10, i)*/
+			divider *= 10;
+			j++;
+		}
+		count += _putchar('0' + abs_value / divider);/*Print the first digit*/
+		/* get the part of the number that we have not printed*/
+		abs_value = abs_value - ((abs_value / divider) * divider);
+	}
+	return (count);
 }
