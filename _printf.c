6a9d68d37e7fa3bf7b8b5dbe0a067beef07bee6f
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-19 16:14:58 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -14,7 +14,6 @@ int _printf(const char *format, ...)
 	int i = 0, count = 0;
 	
 	va_start(x, format);
-	
 	if (!format || format[i] == '\n' || format[i] == '\0' ||
 	    (format[i] == '%' && !format[i + 1]))
 	{
