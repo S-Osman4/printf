3b954994218f05d5601a97084e9803edb117477b
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-04-19 15:15:25 +0300

    Create selector.c

---
+++ selector.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* selector - main function.
+* @format: The dimension of the parameters passed
+* @...: The parameters to print
+* @i: The pointer of the format position
+* @x: The variable to print
+* Description: This function selects the correct function
+* asked by the user for calls it
+* Return: The total number of characters of the functions called.
+*/
+int selector(int *i, const char *format, va_list x)
+
+    
+  
+  
